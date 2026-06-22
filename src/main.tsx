4b5cba0620a1613435409a5f23014f0d6fc44dd0
passawutaplus <passawut.a.plus@gmail.com>
2026-06-22 20:36:02 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,4 +9,14 @@ installCspReporter();
 void initErrorMonitoring();
 registerPwa();
 
+const initialUrl = `${window.location.origin}${window.location.pathname}`;
+document.head
+  .querySelectorAll<HTMLMetaElement>('meta[property="og:url"]')
+  .forEach((meta) => meta.setAttribute("content", initialUrl));
+document.head
+  .querySelectorAll<HTMLLinkElement>('link[rel="canonical"]')
+  .forEach((link) => {
+    link.href = initialUrl;
+  });
+
 createRoot(document.getElementById("root")!).render(<App />);
